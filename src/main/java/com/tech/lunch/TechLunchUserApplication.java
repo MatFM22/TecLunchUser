05dc1298ac3818b80b118d1122facb17b8f4b713
Arturo2209 <arturobullonsupanta@gmail.com>
2024-12-07 03:27:50 -0500

    Commit de WebConfig

---
+++ src/main/java/com/tech/lunch/TechLunchUserApplication.java
@@ -0,0 +1,13 @@
+package com.tech.lunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLunchUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechLunchUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tech/lunch/TechLunchUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.lunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechLunchUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
